23226b4b0be5825e43d94316e42074884caf6cd5
primathinks <primathinks@gmail.com>
2014-12-17 15:54:10 -0500

    Completed TODO 6 for HW 2

--- Assesments/02/Lesson02/ThirdViewController.swift
+++ Assesments/02/Lesson02/ThirdViewController.swift
@@ -9,6 +9,43 @@
 import UIKit
 
 class ThirdViewController: UIViewController {
+    
+    @IBOutlet weak var numTextField: UITextField!
+    @IBOutlet weak var parityLabel: UILabel!
+    
+    func isValidated() -> Bool{
+        var validated: Bool = true
+        if numTextField.text.isEmpty {
+            validated = false
+        }
+        return validated
+    }
+    
+    func updateLabel(str: String) {
+        parityLabel.text = str
+    }
+    
+    func testParity() {
+        var parity:String = "is "
+        let currNum: Int = numTextField.text?.toInt() ?? 0
+        
+        if currNum % 2 == 0 {
+            parity += "even"
+        } else {
+            parity += "odd"
+        }
+        
+        updateLabel(parity)
+    }
+    
+    @IBAction func buttonTapped(sender: AnyObject) {
+        if isValidated() {
+            testParity()
+        } else {
+            updateLabel("Please enter a number.")
+        }
+    }
+    
 /*
     TODO six: Hook up the number input text field, button and text label to this class. When the button is pressed, a message should be printed to the label indicating whether the number is even.
 
