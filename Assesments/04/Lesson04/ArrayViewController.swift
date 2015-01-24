beefedff1a0569cf2beb6d2ff3acf840df21b1c2
primathinks <primathinks@gmail.com>
2015-01-24 09:46:57 -0500

    Completed TODO one and two of HW04

--- Assesments/04/Lesson04/ArrayViewController.swift
+++ Assesments/04/Lesson04/ArrayViewController.swift
@@ -8,16 +8,57 @@
 
 import UIKit
 
-class ArrayViewController: UIViewController {
+class ArrayViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+    
+    /*
+    TODO one: Add a table view AND a text input box to this view controller, either in code or via the storyboard. Accept keyboard input from the text view when the return key is pressed. Add the string that was entered into the text view into an array of strings (stored in this class).
+    TODO two: Make this class a UITableViewDelegate and UITableViewDataSource that supply the above table view with cells. These cells should correspond to the text entered into the text box. E.g. If the text "one", then "two", then "three" was entered into the text box, there should be three cells in this table view that contain those strings in order.
+    
+    */
+    
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet weak var textField: UITextField!
+    
+    var items: [String] = []
+    
+    // textField delegates
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if textField == self.textField {
+            textField.text = nil
+        }
+    }
 
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if textField == self.textField && !self.textField.text.isEmpty {
+            textField.resignFirstResponder()
+            if let item = textField.text {
+                self.items.append(item)
+                self.tableView.reloadData()
+            }
+        }
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        /*
-        TODO one: Add a table view AND a text input box to this view controller, either in code or via the storyboard. Accept keyboard input from the text view when the return key is pressed. Add the string that was entered into the text view into an array of strings (stored in this class).
-        TODO two: Make this class a UITableViewDelegate and UITableViewDataSource that supply the above table view with cells. These cells should correspond to the text entered into the text box. E.g. If the text "one", then "two", then "three" was entered into the text box, there should be three cells in this table view that contain those strings in order.
-        
-        */
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        textField.delegate = self
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        cell.textLabel?.text = self.items[indexPath.row]
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    
 }
 
