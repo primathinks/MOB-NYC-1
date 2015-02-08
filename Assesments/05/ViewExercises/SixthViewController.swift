03f99be5610d25e09c2794cd5c0e10b06e19b37d
primathinks <primathinks@gmail.com>
2015-02-08 03:41:18 -0500

    Hacked my way through todo six. Kinda ugly but it works.

--- Assesments/05/ViewExercises/SixthViewController.swift
+++ Assesments/05/ViewExercises/SixthViewController.swift
@@ -6,6 +6,12 @@
 //  Copyright (c) 2014 Rudd Taylor. All rights reserved.
 //
 
+/* TODO:
+The table view cells on this screen are blank.
+
+Add a label to each cell that is green and centered, and have its text say “Row {X}” (X is the row number of the cell). The tableview should rotate correctly. Use Autolayout.
+*/
+
 import UIKit
 
 class SixthViewController: ExerciseViewController, UITableViewDelegate, UITableViewDataSource {
@@ -20,6 +26,10 @@ class SixthViewController: ExerciseViewController, UITableViewDelegate, UITableV
         self.exerciseView.addSubview(tableView)
         self.tableView.frame = self.exerciseView.frame
         self.tableView.contentInset = UIEdgeInsetsMake(CGRectGetMaxY(self.navigationController!.navigationBar.frame), 0, 0, 0)
+        
+        // removes warning: Detected a case where constraints ambiguously suggest a height of zero...
+        self.tableView.rowHeight = 44.0
+        
         self.tableView.autoresizingMask = self.exerciseView.autoresizingMask
     }
     
@@ -37,14 +47,26 @@ class SixthViewController: ExerciseViewController, UITableViewDelegate, UITableV
             cell = UITableViewCell(style: .Default, reuseIdentifier: "Cell")
         }
         
+        var containerCell = UIView(frame: CGRect(x: 0, y: 0, width: cell!.contentView.frame.width, height: cell!.contentView.frame.height))
+        containerCell.autoresizingMask = self.tableView.autoresizingMask
         
+        var greenLabel = UILabel()
+        greenLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        greenLabel.backgroundColor = UIColor.greenColor()
+        greenLabel.text = "Row \(indexPath.row + 1)"
+        greenLabel.textColor = UIColor.whiteColor()
+        greenLabel.textAlignment = NSTextAlignment.Center
         
-        /* TODO:
-        The table view cells on this screen are blank.
+        containerCell.addSubview(greenLabel)
         
-        Add a label to each cell that is green and centered, and have its text say “Row {X}” (X is the row number of the cell). The tableview should rotate correctly. Use Autolayout.
-        */
-        cell!.textLabel?.text = "Row \(indexPath.row + 1)"
+        // greenLabel constraints
+        containerCell.addConstraint(NSLayoutConstraint(item: greenLabel, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: containerCell, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0))
+        containerCell.addConstraint(NSLayoutConstraint(item: greenLabel, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: containerCell, attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: 0))
+        containerCell.addConstraint(NSLayoutConstraint(item: greenLabel, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: 100))
+        containerCell.addConstraint(NSLayoutConstraint(item: greenLabel, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: 44))
+        
+        cell!.contentView.addSubview(containerCell)
+        
         return cell!
     }
     
