af76966372073b63bb886306b7ec00e0fd0265d6
primathinks <primathinks@gmail.com>
2015-02-24 16:14:21 -0500

    Completed TODO 1 & 2

--- Assesments/07/Lesson07/Lesson07/FirstViewController.swift
+++ Assesments/07/Lesson07/Lesson07/FirstViewController.swift
@@ -10,7 +10,22 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var FirstTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        NSUserDefaults.standardUserDefaults().setValue("Hello World", forKey: "test_string")
+        NSUserDefaults.standardUserDefaults().setInteger(2015, forKey: "test_number")
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let num = NSUserDefaults.standardUserDefaults().integerForKey("test_number")
+        
+        if let str = NSUserDefaults.standardUserDefaults().stringForKey("test_string") {
+            FirstTextView.text = "\(str) \n\(num)"
+        }
+        
     }
 }
--- Assesments/07/Lesson07/Lesson07/SecondViewController.swift
+++ Assesments/07/Lesson07/Lesson07/SecondViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var secondTextView: UITextView!
+    
+    //Go To Settings
+    @IBAction func didTapSettings(sender: AnyObject) {
+        if let appstr = UIApplicationOpenSettingsURLString {
+            if let fileUrl = NSURL(string: appstr) {
+                UIApplication.sharedApplication().openURL(fileUrl)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let sliderValue = NSUserDefaults.standardUserDefaults().floatForKey("settings_slider")
+        if let name = NSUserDefaults.standardUserDefaults().stringForKey("settings_name") {
+            secondTextView.text = "Name: \(name) \nSlider Value: \(sliderValue)"
+        } else {
+            secondTextView.text = "Please update your settings."
+        }
     }
+    
 }
