c5280adec861fb68fe24c367bda06c37a7e3c6b1
primathinks <primathinks@gmail.com>
2015-02-25 16:00:15 -0500

    Completed TODO 5 & 6

--- Assesments/07/Lesson07/Lesson07/FifthViewController.swift
+++ Assesments/07/Lesson07/Lesson07/FifthViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class FifthViewController: UIViewController {
 
+    @IBOutlet weak var FifthTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let documentPath = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first as? NSURL {
+            
+            let filePath = documentPath.URLByAppendingPathComponent("sample.plist", isDirectory: false)
+            
+            let str = FifthTextView.text
+            var displayArray = [""]
+            for char in str {
+                if char != " " {
+                    var item = String(char)
+                    displayArray.append(item)
+                }
+            }
+          
+            displayArray.removeAtIndex(0)
+            (displayArray as NSArray).writeToURL(filePath, atomically: true)
+            
+        } // end documentPath
+        
     }
 }
--- Assesments/07/Lesson07/Lesson07/SixthViewController.swift
+++ Assesments/07/Lesson07/Lesson07/SixthViewController.swift
@@ -11,9 +11,23 @@ import UIKit
 
 class SixthViewController: UIViewController {
 
+    @IBOutlet weak var SixthTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let documentPath = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first as? NSURL {
+            
+            let filePath = documentPath.URLByAppendingPathComponent("sample.plist", isDirectory: false)
+            
+            if let arrayFromFile = NSArray(contentsOfURL: filePath) {
+                var stringArray = arrayFromFile as AnyObject as [String]
+                let str = " ".join(stringArray)
+                SixthTextView.text = str
+            }
+            
+            
+            
+        } // end documentPath
     }
 }
