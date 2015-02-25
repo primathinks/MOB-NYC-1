a284c31d25cb3dce6473517e05b76c33e3ee82a1
primathinks <primathinks@gmail.com>
2015-02-25 15:01:34 -0500

    Completed TODO 3 & 4

--- Assesments/07/Lesson07/Lesson07/FourthViewController.swift
+++ Assesments/07/Lesson07/Lesson07/FourthViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class FourthViewController: UIViewController {
 
+    @IBOutlet weak var FourthTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        readText()
+    }
+    
+    func readText() {
+        if let documentPath = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first as? NSURL {
+            
+            let filePath = documentPath.URLByAppendingPathComponent("sample.txt", isDirectory: false)
+            
+            if let fileContent = NSString(contentsOfURL: filePath, encoding: NSUTF8StringEncoding, error: nil) {
+                FourthTextView.text = fileContent
+            }
+            
+        } // end documentPath
     }
 }
--- Assesments/07/Lesson07/Lesson07/ThirdViewController.swift
+++ Assesments/07/Lesson07/Lesson07/ThirdViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ThirdViewController: UIViewController {
-
+    
+    @IBOutlet weak var ThirdTextView: UITextView!
+    
+    @IBAction func didTapNext(sender: AnyObject) {
+        println("saveText")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        saveText()
+    }
+    
+    func saveText() {
+        if let documentPath = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first as? NSURL {
+            
+            let filePath = documentPath.URLByAppendingPathComponent("sample.txt", isDirectory: false)
+            let displayText = NSString(string: ThirdTextView.text)
+            
+            displayText.writeToURL(filePath, atomically: true, encoding: NSUTF8StringEncoding, error: nil)
+            
+        } // end documentPath
     }
 }
